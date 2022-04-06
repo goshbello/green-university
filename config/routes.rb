adb8e3cadc2ce67d5c960d14a95c357c8415e1d5
goshbello <44555654+goshbello@users.noreply.github.com>
2022-04-06 10:57:45 +0100

    add index, about and new course pages and their routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  root to: 'courses#index'
+  
+  get 'courses/new', to: 'courses#new'
+  get '/about', to: 'pages#about'
+  
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
